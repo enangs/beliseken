9860561f357e59b92a196996c78071811b581817
abcd <abcd@MacBook-Pro-abcd.local>
2026-08-25 16:46:00 +0700

    feat: add Facebook Pixel ID 119619438728935 for ad tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -97,7 +97,7 @@ export default function RootLayout({
               t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1234567890');
+              fbq('init', '119619438728935');
               fbq('track', 'PageView');
             `,
           }}
