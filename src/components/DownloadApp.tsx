5e34057eef09a5e384583b8cae86d2cbde894b0c
Codebuff <codebuff@codebuff.com>
2026-09-07 14:38:54 +0700

    Make iPhone mockup taller with bottom cropped like reference design

--- src/components/DownloadApp.tsx
+++ src/components/DownloadApp.tsx
@@ -15,13 +15,13 @@ export default function DownloadApp() {
     <section className="bg-white py-12 md:py-16">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-10 items-center">
-          {/* iPhone Mockup */}
-          <div className="flex justify-center lg:justify-start order-1">
+          {/* iPhone Mockup - cropped at bottom */}
+          <div className="flex justify-center lg:justify-start order-1 overflow-hidden">
             {/* eslint-disable-next-line @next/next/no-img-element */}
             <img
               src="/app-mockup.png"
               alt="BeliSeken App Preview"
-              className="w-[280px] md:w-[320px] h-auto drop-shadow-2xl"
+              className="w-[300px] md:w-[360px] h-auto drop-shadow-2xl translate-y-8"
             />
           </div>
 
